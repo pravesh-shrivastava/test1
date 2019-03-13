c018d3c8b6c5596ba777fb820b9a69705c2bea50
pravesh-shrivastava <40330373+pravesh-shrivastava@users.noreply.github.com>
2019-03-13 14:17:59 +0530

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,4 +1,4 @@
 node {
 	stage 'Run JMeter Test'
-	java -jar D:\apache-jmeter-5.1\bin\ApacheJMeter.jar -n -t D:\JMeterPractice\TC003.jmx -l test1.jtl
+	bat D:\apache-jmeter-5.1\bin\jmeter.jar -n -t D:\JMeterPractice\TC003.jmx -l test1.jtl
 }
